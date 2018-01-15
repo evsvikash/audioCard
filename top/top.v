41778c5eb9aa6d8f08c96a4226cb63def71deaf1
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2018-01-15 21:19:37 +0100

    initialize ULPI regs

--- top/top.v
+++ top/top.v
@@ -72,8 +72,8 @@ ULPI ULPI_0 (
 parameter RESET = `PARAM_SIZE'd1;
 parameter W_FUN_CTRL_REG = `PARAM_SIZE'd2;
 parameter R_FUN_CTRL_REG = `PARAM_SIZE'd3;
-parameter W_OTG_CTRL = `PARAM_SIZE'd4;
-parameter R_OTG_CTRL = `PARAM_SIZE'd5;
+parameter W_OTG_CTRL_REG = `PARAM_SIZE'd4;
+parameter R_OTG_CTRL_REG = `PARAM_SIZE'd5;
 parameter W_SCR_REG  = `PARAM_SIZE'd6;
 parameter R_SCR_REG  = `PARAM_SIZE'd7;
 parameter WAIT_RD = `PARAM_SIZE'd8;
@@ -85,7 +85,7 @@ parameter FUN_CTRL_REG = `REG_MAP_SIZE'h04;
 parameter OTG_CTRL_REG  = `REG_MAP_SIZE'h0A;
 parameter SCRATCH_REG   = `REG_MAP_SIZE'h16;
 
-reg [`PARAM_SIZE - 1 : 0] state;
+reg [`PARAM_SIZE - 1 : 0] state, next_state, previous_state;
 reg [7 : 0] ulpi_reg_data_o, ulpi_rxcmd_o;
 reg [12 : 0] scratch_wr_rd;
 
@@ -93,6 +93,8 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 	if (!NRST_A_USB) begin
 
 		state <= RESET;
+		next_state <= RESET;
+		previous_state <= RESET;
 		ulpi_reg_data_o <= 8'd0;
 		ulpi_rxcmd_o <= 8'd0;
 		scratch_wr_rd <= 0;
@@ -102,49 +104,62 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 		ulpi_rxcmd_o <= ulpi_rxcmd_a;
 	
 		if (!ulpi_ready_a) begin
+
 			state <= RESET;
+			next_state <= RESET;
+			previous_state <= RESET;
+
 		end else begin
 	
 			case (state)
 			RESET: begin
-				state <= IDLE;
+				state <= W_OTG_CTRL_REG;
 			end
 			IDLE: begin
 				scratch_wr_rd <= scratch_wr_rd + 1;
-				if (scratch_wr_rd == 0)
-					state <= W_SCR_REG;
-				else if (scratch_wr_rd == 1)
+				if (scratch_wr_rd == 1)
 					state <= R_SCR_REG;
-				else if (scratch_wr_rd == 128)
-					state <= W_FUN_CTRL_REG;
 				else if (scratch_wr_rd == 2048)
 					state <= R_FUN_CTRL_REG;
 			end
+			W_OTG_CTRL_REG: begin
+				state <= WAIT_WR; 
+				next_state <= W_FUN_CTRL_REG;
+				previous_state <= state;
+			end
 			W_FUN_CTRL_REG: begin
 				state <= WAIT_WR;
+				next_state <= IDLE;
+				previous_state <= state;
 			end
 			R_FUN_CTRL_REG: begin
 				state <= WAIT_RD;
+				next_state <= IDLE;
+				previous_state <= state;
 			end
 			W_SCR_REG: begin
 				state <= WAIT_WR;
+				next_state <= IDLE;
+				previous_state <= state;
 			end
 			R_SCR_REG: begin
 				state <= WAIT_RD;
+				next_state <= IDLE;
+				previous_state <= state;
 			end
 			WAIT_WR: begin
 				if (ulpi_reg_done_a) begin
-					state <= IDLE;
+					state <= next_state;
 				end else if (ulpi_reg_fail_a) begin
-					state <= IDLE;
+					state <= previous_state;
 				end
 			end
 			WAIT_RD: begin
 				if (ulpi_reg_done_a) begin
-					state <= IDLE;
+					state <= next_state;
 					ulpi_reg_data_o <= ulpi_reg_data_o_a;
 				end else if (ulpi_reg_fail_a) begin
-					state <= IDLE;
+					state <= previous_state;
 				end
 			end
 			default: begin
@@ -218,6 +233,12 @@ always @(state, small_cnt) begin
 		ulpi_reg_data_i_a = 0;
 		ulpi_reg_rw_a = 1'b0;
 		ulpi_reg_en_a = 1'b1;
+	end
+	W_OTG_CTRL_REG: begin
+		ulpi_reg_addr_a = OTG_CTRL_REG;
+		ulpi_reg_data_i_a = 0;
+		ulpi_reg_rw_a = 1'b1;
+		ulpi_reg_en_a = 1'b1; 
 	end
 	W_SCR_REG: begin
 		ulpi_reg_addr_a = SCRATCH_REG;
