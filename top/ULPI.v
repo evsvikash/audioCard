bed0b2010629d42c57265d3902fe04a01f11b40f
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2018-01-28 14:53:33 +0100

    Added restart through ULPI fun_ctrl_reg; Added more steps into speed handshake procedure.

--- top/ULPI.v
+++ top/ULPI.v
@@ -38,9 +38,9 @@ module ULPI (
 	output [7:0] USB_DATA_OUT,
 	output USB_DATA_OUT_STRB,
 	output USB_DATA_OUT_END,
-	output USB_DATA_OUT_FAIL //WARNING! The same behavior as in REG_FAIL output
+	output USB_DATA_OUT_FAIL, //WARNING! The same behavior as in REG_FAIL output
 	
-	//output [7:0] LED
+	output [7:0] STATE
 );
 
 `define PARAM_SIZE 8
@@ -50,22 +50,26 @@ parameter IDLE	      	= `PARAM_SIZE'd1;
 parameter REG_WRITE  	= `PARAM_SIZE'd2;
 parameter REG_WRITE_DATA = `PARAM_SIZE'd3;
 parameter REG_WRITE_END = `PARAM_SIZE'd4;
-parameter REG_READ      = `PARAM_SIZE'd5;
-parameter REG_READ_DATA = `PARAM_SIZE'd6;
-parameter REG_READ_END = `PARAM_SIZE'd7;
+//parameter REG_READ      = `PARAM_SIZE'd5;
+//parameter REG_READ_DATA = `PARAM_SIZE'd6;
+//parameter REG_READ_END = `PARAM_SIZE'd7;
 parameter POST_RESET    = `PARAM_SIZE'd9;
 parameter READ_DATA = `PARAM_SIZE'd10;
 parameter READ_DATA_END = `PARAM_SIZE'd11;
 parameter WRITE_DATA_PID = `PARAM_SIZE'd12;
 parameter WRITE_DATA = `PARAM_SIZE'd13;
 parameter WRITE_DATA_END = `PARAM_SIZE'd14;
+parameter UTMI_RESET = `PARAM_SIZE'd15;
 
 `define REG_MAP_SIZE 6
 parameter FUNC_CTRL_REG = `REG_MAP_SIZE'h04;
 
-reg [`PARAM_SIZE - 1 : 0] state;
+reg [`PARAM_SIZE - 1 : 0] state, next_state;
 reg [7:0] reg_val, rxcmd, usb_data_i_reg, usb_data_o_reg;
 reg [5:0] reg_addr;
+
+reg reg_en, reg_rw;
+reg usb_data_o_start;
 
 reg last_usb_dir, last_usb_nxt, usb_data_o_get_next, usb_data_i_set_next;
 
@@ -73,12 +77,19 @@ wire [7:0] USB_DATA_I, USB_DATA_O;
 
 wire now_write_a = !USB_DIR & !last_usb_dir;
 reg usb_stupid_test;
+
+assign STATE = state;
+
 always @(posedge CLK_60M, negedge NRST_A_USB) begin
 	if (!NRST_A_USB) begin
 		state <= RESET;
+		next_state <= RESET;
+
 		rxcmd <= 8'd0;
 		reg_val <= 8'd0;
 		reg_addr <= 6'd0;
+		reg_en <= 0;
+		reg_rw <= 0;
 
 		last_usb_dir <= 1'b0;
 		last_usb_nxt <= 1'b0;	
@@ -89,14 +100,35 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 		usb_stupid_test <= 1'b0;
 		usb_data_o_get_next <= 0;
 		usb_data_i_set_next <= 0;
+		usb_data_o_start <= 0;
 	end else begin
-		
 		last_usb_dir <= USB_DIR;
-		last_usb_nxt <= USB_NXT;
+		last_usb_nxt <= USB_NXT;	
+
+		usb_data_o_get_next <= 0;
+
+		// register operations/usb data write high-tech "scheduling" system
+		if (REG_EN && !reg_en) begin
+			reg_en <= 1;
+			reg_val <= REG_DATA_I;
+			reg_addr <= REG_ADDR;
+			reg_rw <= REG_RW;
+		end
+
+		if (USB_DATA_IN_START_END && !usb_data_o_start) begin
+			usb_data_o_reg <= USB_DATA_IN;
+			usb_data_o_get_next <= 1;
+			usb_data_o_start <= 1;	
+		end
+		// end of RO/USBWHT"SCH"SYS -----------------------------------
 
 		case (state)
 		RESET: begin
 			state <= POST_RESET;
+		end
+		UTMI_RESET: begin
+			if (USB_DIR)
+				state <= POST_RESET;
 		end
 		POST_RESET: begin
 			if (!last_usb_dir & !USB_DIR) begin
@@ -109,26 +141,24 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 
 			if (USB_DIR) begin
 				state <= READ_DATA;
-			end else if (USB_DATA_IN_START_END) begin
-				usb_data_o_reg <= USB_DATA_IN;
-				usb_data_o_get_next <= 1;
-
+			end else if (usb_data_o_start) begin
 			 	state <= WRITE_DATA_PID;
-			end else if (REG_EN) begin
-				case (REG_RW)
-				1'b0: begin
-					reg_val <= 8'd0;
-					reg_addr <= REG_ADDR;
-					state <= REG_READ;
-				end
-				1'b1: begin
-					reg_val <= REG_DATA_I;
-					reg_addr <= REG_ADDR;
+			end else if (reg_en) begin
+				reg_en <= 0;
+//				case (reg_rw)
+//				1'b0: begin
+//					state <= REG_READ;
+//				end
+//				1'b1: begin
 					state <= REG_WRITE;
-				end
-				default: begin
-				end
-				endcase
+					next_state <= IDLE;
+
+					if (reg_addr == 8'h04 && (reg_val & 8'b00100000))
+						next_state <= UTMI_RESET;
+//				end
+//				default: begin
+//				end
+//				endcase
 			end
 		end
 		REG_WRITE: begin
@@ -139,7 +169,7 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 				usb_stupid_test <= 1'b1;
 
 			end else begin
-				state <= READ_DATA;
+				state <= IDLE; //TODO
 			end
 		end
 		REG_WRITE_DATA: begin
@@ -148,13 +178,13 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 					state <= REG_WRITE_END;
 				end
 			end else begin
-				state <= READ_DATA;
+				state <= IDLE; //TODO
 			end
 		end
 		REG_WRITE_END: begin
-			state <= IDLE;
+			state <= next_state;
 		end
-		REG_READ: begin
+/*		REG_READ: begin
 			if (!last_usb_dir & !USB_DIR) begin
 				if (USB_NXT) begin
 					state <= REG_READ_DATA;
@@ -173,7 +203,7 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 		end
 		REG_READ_END: begin
 			state <= IDLE;
-		end
+		end*/
 		WRITE_DATA_PID: begin
 			usb_data_o_get_next <= 0;
 
@@ -185,7 +215,7 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 				end
 				usb_stupid_test <= 1'b1;
 			end else begin
-				state <= READ_DATA; 
+				state <= IDLE; //TODO 
 			end
 		end
 		
@@ -193,9 +223,10 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 			usb_data_o_get_next <= 0;
 
 			if (USB_DIR) begin
-				state <= READ_DATA;
-			end else if (USB_DATA_IN_START_END & USB_NXT) begin
+				state <= IDLE; //TODO
+			end else if (USB_DATA_IN_START_END) begin //TODO
 				state <= WRITE_DATA_END;
+				usb_data_o_start <= 0;
 			end else if (USB_NXT) begin
 				usb_data_o_reg <= USB_DATA_IN;
 				usb_data_o_get_next <= 1;
@@ -215,7 +246,7 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 					usb_data_i_reg <= 0;
 					rxcmd <= USB_DATA_I;
 					if (USB_DATA_I & 8'b00100000)
-						state <= READ_DATA;
+						state <= IDLE; //TODO
 				end else begin
 					usb_data_i_reg <= USB_DATA_I;
 					usb_data_i_set_next <= 1;		
@@ -342,7 +373,7 @@ always @(NRST_A_USB, state, reg_addr, reg_val, rxcmd, last_usb_nxt, usb_data_i_r
 		USB_DATA_OUT_FAIL_a = 0;
 		USB_DATA_OUT_a = 0;
 	end
-	REG_READ: begin //send TXCMD
+/*	REG_READ: begin //send TXCMD
 		ready_a = 1'b1;
 
 		USB_STP_a = 1'b0;
@@ -401,7 +432,7 @@ always @(NRST_A_USB, state, reg_addr, reg_val, rxcmd, last_usb_nxt, usb_data_i_r
 		USB_DATA_OUT_END_a = 0;
 		USB_DATA_OUT_FAIL_a = 0;
 		USB_DATA_OUT_a = 0;
-	end
+	end*/
 	IDLE: begin
 		ready_a = 1'b1;
 
@@ -430,16 +461,16 @@ always @(NRST_A_USB, state, reg_addr, reg_val, rxcmd, last_usb_nxt, usb_data_i_r
 		
 		REG_DATA_O_a = 8'd0;
 		REG_DONE_a = 1'b0;
-		REG_FAIL_a = 1;
+		REG_FAIL_a = 0;
 		
 		RXCMD_a = rxcmd;
 
 		USB_DATA_IN_STRB_a = 0;
-		USB_DATA_IN_FAIL_a = 1;
+		USB_DATA_IN_FAIL_a = 0;
 
 		USB_DATA_OUT_STRB_a = usb_data_i_set_next;
 		USB_DATA_OUT_END_a = 0;
-		USB_DATA_OUT_FAIL_a = 1;
+		USB_DATA_OUT_FAIL_a = 0;
 		USB_DATA_OUT_a = usb_data_i_reg;			
 	end
 	READ_DATA_END: begin
@@ -522,6 +553,26 @@ always @(NRST_A_USB, state, reg_addr, reg_val, rxcmd, last_usb_nxt, usb_data_i_r
 		USB_DATA_OUT_FAIL_a = 0;
 		USB_DATA_OUT_a = 0;	
 	end
+	UTMI_RESET: begin
+		ready_a = 1'b0;
+
+		USB_STP_a = 1'b0;
+		USB_DATA_O_a = 8'd0;
+
+		REG_DATA_O_a = 8'd0;
+		REG_DONE_a = 1'b0;
+		REG_FAIL_a = 1'b0;
+
+		RXCMD_a = rxcmd;
+
+		USB_DATA_IN_STRB_a = 0;
+		USB_DATA_IN_FAIL_a = 0;
+
+		USB_DATA_OUT_STRB_a = 0;
+		USB_DATA_OUT_END_a = 0;
+		USB_DATA_OUT_FAIL_a = 0;
+		USB_DATA_OUT_a = 0;	
+	end
 	default: begin
 		ready_a = 1'b0;
 
@@ -563,8 +614,6 @@ assign USB_DATA_OUT_STRB = USB_DATA_OUT_STRB_a;
 assign USB_DATA_OUT_END = USB_DATA_OUT_END_a;
 assign USB_DATA_OUT_FAIL = USB_DATA_OUT_FAIL_a;
 assign USB_DATA_OUT = USB_DATA_OUT_a;
-
-//assign LED = state;
 
 assign USB_DATA_I = USB_DATA;
 assign USB_DATA = (now_write_a == 1'b1) ? USB_DATA_O : 8'hzz;
--- top/top.v
+++ top/top.v
@@ -36,6 +36,8 @@ wire ulpi_usb_data_i_strb_a, ulpi_usb_data_i_fail_a;
 wire [7:0] ulpi_usb_data_o_a;
 wire ulpi_usb_data_o_strb_a, ulpi_usb_data_o_end_a, ulpi_usb_data_o_fail_a;
 
+wire[7:0] ulpi_state;
+
 ULPI ULPI_0 (
 	.CLK_60M(CLK_60M),
 	.NRST_A_USB(NRST_A_USB),
@@ -61,7 +63,8 @@ ULPI ULPI_0 (
 	.USB_DATA_OUT(ulpi_usb_data_o_a),
 	.USB_DATA_OUT_STRB(ulpi_usb_data_o_strb_a),
 	.USB_DATA_OUT_END(ulpi_usb_data_o_end_a),
-	.USB_DATA_OUT_FAIL(ulpi_usb_data_o_fail_a)
+	.USB_DATA_OUT_FAIL(ulpi_usb_data_o_fail_a),
+	.STATE(ulpi_state)
 );
 
 //-----------------------------------------------------------------------------
@@ -80,6 +83,11 @@ parameter DETECT_SE0 = `PARAM_SIZE'd11;
 parameter TXCMD_CHIRP_K = `PARAM_SIZE'd12;
 parameter TXCMD_CHIRP_K_START = `PARAM_SIZE'd13;
 parameter TXCMD_CHIRP_K_END = `PARAM_SIZE'd14;
+parameter DETECT_K = `PARAM_SIZE'd15;
+parameter DETECT_J = `PARAM_SIZE'd16;
+parameter SET_ULPI_START = `PARAM_SIZE'd17;
+parameter SET_ULPI_CHIRP = `PARAM_SIZE'd18;
+parameter FAIL = `PARAM_SIZE'b01010101;
 
 `define REG_MAP_SIZE 6
 parameter FUN_CTRL_REG = `REG_MAP_SIZE'h04;
@@ -88,8 +96,9 @@ parameter SCRATCH_REG   = `REG_MAP_SIZE'h16;
 
 
 reg [`PARAM_SIZE - 1 : 0] state, next_state;
-reg [7 : 0] ulpi_reg_data_o, ulpi_rxcmd_o;
+reg [7 : 0] ulpi_reg_data_o, ulpi_rxcmd_o, fun_ctrl_reg_val;
 reg [1:0] cnt;
+reg [1:0] jk_trans;
 
 reg clk_10MHz;
 reg [15:0] clk_10MHz_cnt;
@@ -103,9 +112,12 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 
 		ulpi_reg_data_o <= 0;
 		ulpi_rxcmd_o <= 0;
+		fun_ctrl_reg_val <= 0;
+
 		cnt <= 0;
 		clk_10MHz <= 0;
 		clk_10MHz_cnt <= 0;
+		jk_trans <= 0;
 	end else begin
 
 		ulpi_rxcmd_o <= ulpi_rxcmd_a;
@@ -121,78 +133,119 @@ always @(posedge CLK_60M, negedge NRST_A_USB) begin
 		end		
 //-----------------------------------------------------------------------------	
 
-		if (ulpi_ready_a) begin
 	
-			case (state)
-			RESET: begin
+		case (state)
+		RESET: begin
+			if (ulpi_ready_a) begin
 				state <= W_OTG_CTRL_REG;
+				next_state <= SET_ULPI_START;
+				clk_10MHz_cnt <= 0;
+			end
+		end
+		SET_ULPI_START : begin
+			if (clk_10MHz_cnt == 65000) begin
+				fun_ctrl_reg_val <= 8'b01100101;
+				state <= W_FUN_CTRL_REG;
 				next_state <= DETECT_SE0;
 			end
-			DETECT_SE0: begin
-				//we should clk_10MHz_cnt reset here, not in WAIT_WR...
-				if (ulpi_rxcmd_o[1:0] != 2'b00) begin
-					clk_10MHz_cnt <= 0; 
-				end else if (clk_10MHz_cnt > 25) begin
-					state <= W_FUN_CTRL_REG;
-					next_state <= TXCMD_CHIRP_K_START;
+		end	
+		DETECT_SE0: begin
+			if (ulpi_rxcmd_o[1:0] != 2'b00 || !ulpi_ready_a) begin
+				clk_10MHz_cnt <= 0; 
+			end else if (clk_10MHz_cnt > 25) begin
+				state <= SET_ULPI_CHIRP;
+			end
+		end
+		SET_ULPI_CHIRP: begin
+			fun_ctrl_reg_val <= 8'b01010100;
+			state <= W_FUN_CTRL_REG;
+			next_state <= TXCMD_CHIRP_K_START;
+		end
+		TXCMD_CHIRP_K_START: begin
+			clk_10MHz_cnt <= 0;
+			state <= TXCMD_CHIRP_K;
+		end	
+		TXCMD_CHIRP_K: begin
+			if (clk_10MHz_cnt > 20000)
+				state <= TXCMD_CHIRP_K_END;
+			if (ulpi_usb_data_i_fail_a)
+				state <= FAIL;
+		end
+		TXCMD_CHIRP_K_END: begin
+			clk_10MHz_cnt <= 0;
+			state <= DETECT_K; //now we will be detecting K-J-K-J-L-J sequence
+		end
+		DETECT_K: begin
+			if (ulpi_rxcmd_o[1:0] == 2'b10) begin
+				if (clk_10MHz_cnt > 30) begin
+					clk_10MHz_cnt <= 0;
+					state <= DETECT_J;
 				end
-			end
-			TXCMD_CHIRP_K_START: begin
+			end else begin
 				clk_10MHz_cnt <= 0;
-				state <= TXCMD_CHIRP_K;
-			end	
-			TXCMD_CHIRP_K: begin
-				if (clk_10MHz_cnt > 20000)
-					state <= TXCMD_CHIRP_K_END;
 			end
-			TXCMD_CHIRP_K_END: begin
+		end
+		DETECT_J: begin
+			if (ulpi_rxcmd_o[1:0] == 2'b01) begin
+				if (clk_10MHz_cnt > 30) begin
+					if (jk_trans == 2) begin
+						state <= IDLE;		
+					end else begin
+						clk_10MHz_cnt <= 0;
+						state <= DETECT_K;
+						jk_trans <= jk_trans + 1;
+					end
+				end
+			end else begin
+				clk_10MHz_cnt <= 0;
+			end
+		end
+		W_OTG_CTRL_REG: begin
+			state <= WAIT_WR; 
+		end
+/*		R_OTG_CTRL_REG: begin
+			state <= WAIT_RD;
+		end*/
+		W_FUN_CTRL_REG: begin
+			state <= WAIT_WR;
+		end
+/*		R_FUN_CTRL_REG: begin
+			state <= WAIT_RD;
+		end*/
+/*		W_SCR_REG: begin
+			state <= WAIT_WR;
+		end
+		R_SCR_REG: begin
+			state <= WAIT_RD;
+		end*/
+		IDLE: begin
+		end
+		FAIL: begin
+		end
+		WAIT_WR: begin
+			clk_10MHz_cnt <= 0; //not the best place
+			if (ulpi_reg_done_a) begin
+				state <= next_state;
+			end else if (ulpi_reg_fail_a) begin
 				state <= IDLE;
 			end
-			W_OTG_CTRL_REG: begin
-				state <= WAIT_WR; 
-			end
-/*			R_OTG_CTRL_REG: begin
-				state <= WAIT_RD;
-			end*/
-			W_FUN_CTRL_REG: begin
-				state <= WAIT_WR;
-			end
-/*			R_FUN_CTRL_REG: begin
-				state <= WAIT_RD;
-			end*/
-/*			W_SCR_REG: begin
-				state <= WAIT_WR;
-			end
-			R_SCR_REG: begin
-				state <= WAIT_RD;
-			end*/
-			IDLE: begin
-			end
-			WAIT_WR: begin
-				clk_10MHz_cnt <= 0; //not the best place
-				if (ulpi_reg_done_a) begin
-					state <= next_state;
-				end else if (ulpi_reg_fail_a) begin
-					state <= IDLE;
-				end
-			end
-			WAIT_RD: begin
-				if (ulpi_reg_done_a) begin
-					state <= IDLE;
-					ulpi_reg_data_o <= ulpi_reg_data_o_a;
-				end else if (ulpi_reg_fail_a) begin
-					state <= IDLE;
-				end
-			end
-			default: begin
-				state <= RESET;
-			end
-			endcase
 		end
+		WAIT_RD: begin
+			if (ulpi_reg_done_a) begin
+				state <= IDLE;
+				ulpi_reg_data_o <= ulpi_reg_data_o_a;
+			end else if (ulpi_reg_fail_a) begin
+				state <= IDLE;
+			end
+		end
+		default: begin
+			state <= RESET;
+		end
+		endcase
 	end
 end
 
-always @(state) begin
+always @(state, fun_ctrl_reg_val) begin
 	case (state)
 	RESET: begin
 		ulpi_reg_addr_a = 6'd0;
@@ -248,9 +301,9 @@ always @(state) begin
 		ulpi_usb_data_i_a = 0;
 		ulpi_usb_data_i_start_end_a = 0;
 	end
-	W_FUN_CTRL_REG: begin // <-- can not write this register
+	W_FUN_CTRL_REG: begin
 		ulpi_reg_addr_a = FUN_CTRL_REG;
-		ulpi_reg_data_i_a = 8'b01010100;
+		ulpi_reg_data_i_a = fun_ctrl_reg_val;
 		ulpi_reg_rw_a = 1'b1;
 		ulpi_reg_en_a = 1'b1;
 
@@ -268,6 +321,24 @@ always @(state) begin
 		ulpi_reg_data_i_a = 8'b00000000;
 		ulpi_reg_rw_a = 1'b1;
 		ulpi_reg_en_a = 1'b1; 
+
+		ulpi_usb_data_i_a = 0;
+		ulpi_usb_data_i_start_end_a = 0;
+	end
+	DETECT_K: begin
+		ulpi_reg_addr_a = 6'd0;
+		ulpi_reg_data_i_a = 8'd0;
+		ulpi_reg_rw_a = 1'b0;
+		ulpi_reg_en_a = 1'b0;
+
+		ulpi_usb_data_i_a = 0;
+		ulpi_usb_data_i_start_end_a = 0;
+	end
+	DETECT_J: begin
+		ulpi_reg_addr_a = 6'd0;
+		ulpi_reg_data_i_a = 8'd0;
+		ulpi_reg_rw_a = 1'b0;
+		ulpi_reg_en_a = 1'b0;
 
 		ulpi_usb_data_i_a = 0;
 		ulpi_usb_data_i_start_end_a = 0;
@@ -308,7 +379,34 @@ always @(state) begin
 		ulpi_usb_data_i_a = 0;
 		ulpi_usb_data_i_start_end_a = 0;
 	end
+	SET_ULPI_CHIRP: begin
+		ulpi_reg_addr_a = 6'd0;
+		ulpi_reg_data_i_a = 8'd0;
+		ulpi_reg_rw_a = 1'b0;
+		ulpi_reg_en_a = 1'b0;
+
+		ulpi_usb_data_i_a = 0;
+		ulpi_usb_data_i_start_end_a = 0;
+	end	
+	SET_ULPI_START: begin
+		ulpi_reg_addr_a = 6'd0;
+		ulpi_reg_data_i_a = 8'd0;
+		ulpi_reg_rw_a = 1'b0;
+		ulpi_reg_en_a = 1'b0;
+
+		ulpi_usb_data_i_a = 0;
+		ulpi_usb_data_i_start_end_a = 0;
+	end
 	WAIT_RD: begin
+		ulpi_reg_addr_a = 6'd0;
+		ulpi_reg_data_i_a = 8'd0;
+		ulpi_reg_rw_a = 1'b0;
+		ulpi_reg_en_a = 1'b0;
+
+		ulpi_usb_data_i_a = 0;
+		ulpi_usb_data_i_start_end_a = 0;
+	end
+	FAIL: begin
 		ulpi_reg_addr_a = 6'd0;
 		ulpi_reg_data_i_a = 8'd0;
 		ulpi_reg_rw_a = 1'b0;
