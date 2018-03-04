6de5f6f044ccc4c60d6fd31031986933ac59a485
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2018-03-04 11:58:43 +0100

    Add tests to endpoint_ctrl_tb.v

--- top/endpoint_ctrl_tb.v
+++ top/endpoint_ctrl_tb.v
@@ -1,3 +1,4 @@
+`timescale 1 ps / 1 ps
 module endpoint_ctrl_tb (
 	output clk
 );
@@ -13,7 +14,6 @@ always begin
 	end
 end
 
-reg [7:0] data;
 always begin
 	nrst <= 1'b0;
 	#10;
@@ -27,7 +27,7 @@ end
 reg [23:0] token_in;
 reg token_in_strb, data_in_strb, data_in_end, data_in_fail, data_o_strb, data_o_fail;
 reg [7:0] data_in, pid;
-wire data_o;
+wire [7:0] data_o;
 
 endpoint_ctrl DUT (
 	.nrst(nrst),
@@ -46,10 +46,71 @@ endpoint_ctrl DUT (
 );
 
 always begin
-	#100;
-	token_in <= 24'b111110000000000000101101;
-	token_in_strb <= 1;
-	#10;
+	token_in <= 0;
 	token_in_strb <= 0;
-	#10;
+	data_in <= 0;
+	data_in_strb <= 0;
+	data_in_end <= 0;
+	data_in_fail <= 0;
+	pid <= 0; //TODO !!! TODO
+	data_o_strb <= 0;
+	data_o_fail <= 0;
+		
+	#100;
+	#1;
+	repeat(5) begin
+		token_in <= 24'b111110000000000000101101;
+		token_in_strb <= 1;
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		#10;
+		token_in_strb <= 0;
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		#10;
+		repeat(8) begin
+			data_in <= 8'd5;
+			data_in_strb <= 1;
+			if (data_o != 0) $finish;
+			if (data_o_start_stop != 0) $finish;
+			#10;
+		end
+	
+		data_in_strb <= 0;
+		data_in_end <= 1;
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		#10;
+	
+		data_in_end <= 0;
+		if (data_o != 8'b11010010) $finish; 
+		if (data_o_start_stop != 1) $finish;
+		#10;
+	
+		if (data_o != 0) $finish; 
+		if (data_o_start_stop != 0) $finish;
+		#10;
+	
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		data_o_strb <= 1;
+		#1;
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 1) $finish;
+		#9;
+	
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		data_o_strb <= 0;
+		#10;
+		
+		if (data_o != 0) $finish;
+		if (data_o_start_stop != 0) $finish;
+		#10;
+	end
+
+	#300;
+	$finish;
+	
 end
+endmodule
